fad6fe933d06e08caa1d5d33e9a2f143e23e8351
rnzndrei <renzandreidelrosario@gmail.com>
2025-03-05 07:00:16 +0800

    Added Overview

---
+++ try.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Demo: Using Case Tools (Git as a Version Control)" << endl;
+    cout << "Group 5:" << endl;
+    cout << "Members:" << endl;
+    cout << "Angel Amaya" << endl
+        << "Lyrus Lei" << endl
+        << "Renz Andrei" << endl
+        << "Xyrelle Dominique" << endl;
+
+    cout << endl << "Introduction of Members:" << endl;
+ }
